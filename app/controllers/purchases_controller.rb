de6db250556fc4c0ccf0f9c1f567ba68ec411bef
piyush13 <piyushwww13@gmail.com>
2016-07-05 19:52:38 +0530

    seconf commit all basic functionalities done

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,32 +1,39 @@
+require 'json'
+
 class PurchasesController < ApplicationController
   def new
-  		@param=params[:ItemName]
-      @name=params[:name]
-      @id=params[:id]
-      if !@id
-        @id=0
-      end  
-      @Price=params[:Price].to_i
-      @stock=params[:stock].to_i          
+      
+      require 'json'
+      if params[:total]
+       purchase=Purchase.new(:total => params[:total], :user_id => 1)
+       purchase.save   
+      end    
+      h = JSON.parse params[:cart] rescue {} 
+      h = Array(h)
+      cart =[]
+
+      h.each do |item|
+        h1=item.to_h
+        # update the quantity #>
+        item = Item.find(h1["item_id"])
+        q=(item.stock-h1["quantity"]).to_i
+        item.update(stock: q)
+         # ----- 
+        @item=Cart.new(:total => h1["total"], :purchase_id =>purchase.purchase_id ,
+        :item_id => h1["item_id"],:quantity =>h1["quantity"])
+        @item.save    
+      end
   end
 
   def search
   		param=params[:ItemName]
-		  @items=Item.where("ItemName like ?", "%#{param}%") 	
-      # @item_json=@items.to_json
+		  @items=Item.where("ItemName like ?", "%#{param}%")
+      @item_json=@items.to_json
       # render action: :new
       # redirect_to action: 'new' 
   end
 
-  def search1 
-
-
-  end 
   def list
-      @ItemName=params[:ItemName]
-      @quantity=params[:quantity].to_i
-      @id=params[:item_id]
-      @item=Item.where({:item_id => @id })
-      @total=@item[0].Price * @quantity
+     
   end
 end
